eb0f326f60b61445a4f384e6b870dd99b644a4cd
Motti Bechhofer <mybechhofer@gmail.com>
2024-12-03 10:56:59 +0200

    style nav toggles

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import './App.css';
 import { ConfigEditor } from './components/ConfigEditor/ConfigEditor';
 import { PolicySetsProvider } from './context/PolicySetsContext';
 import { ThemeProvider } from './context/ThemeContext';
-import { ThemeToggle } from './components/common/ThemeToggle';
-import { ModeToggle } from './components/common/ModeToggle';
 import { Analytics } from '@vercel/analytics/react';
 import { ModeProvider } from './context/ModeContext';
+import { Nav } from './components/common/Nav';
 
 function App() {
   return (
@@ -15,11 +14,7 @@ function App() {
         <PolicySetsProvider>
           <div className="App">
             <header className="App-header">
-              <h1>Tail Sampling Config Generator</h1>
-              <div>
-                <ModeToggle />
-                <ThemeToggle />
-              </div>
+              <Nav />
             </header>
             <main>
               <ConfigEditor />
