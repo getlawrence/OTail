9008be6d57abf9beb33b155405f771f5effa9053
Motti Bechhofer <mybechhofer@gmail.com>
2024-12-25 13:39:07 +0200

    fix sidebar width

--- otail/src/layout.tsx
+++ otail/src/layout.tsx
@@ -8,14 +8,14 @@ import {
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider className="flex h-screen">
+    <SidebarProvider className="flex h-screen w-screen">
       <AppSidebar />
-      <SidebarInset className="flex-1 w-[calc(100vw-250px)]">
+      <SidebarInset className="flex-1">
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 w-full">
+        <div className="flex flex-1 flex-col gap-4 p-4 w-full h-full">
           {children}
         </div>
       </SidebarInset>
